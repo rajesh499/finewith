ee1ab7d34569867cbbbb37f852000d5f3fca0771
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2021-12-19 21:34:15 +0530

    Update and rename applog.log to applog.py

---
+++ applog.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import logging
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route('/')
+def hello_world():
+    app.logger.info('Processing default request')
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
