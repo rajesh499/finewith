3cce3aa351781c7cc6cb3807cfe21cb428428894
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2021-12-19 20:57:50 +0530

    Update applog.py

--- applog.py
+++ applog.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+import logging
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello world rajesh is here'
-
-if __name__== "__main__":
-    app.run(host="0.0.0.0", port=80)
+ 
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+ 
+@app.route('/blogs')
+def blog():
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+    return f"Welcome to the Blog"
+ 
+app.run(host='localhost', debug=True)
