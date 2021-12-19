790d7ae75c5ae1bdf2fc833695ca001b42872caf
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2021-12-19 16:49:57 +0530

    Update applog.py

--- applog.py
+++ applog.py
@@ -1,14 +1,10 @@
 from flask import Flask
-import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
-
 @app.route('/')
 def hello_world():
-    app.logger.info('Processing default request')
-    return 'Hello World!'
+    return 'Hello world rajesh is here'
 
-if __name__ == '__main__':
+if __name__== "__main__":
     app.run(host="0.0.0.0", port=80)
