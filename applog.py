b7944efb76b461e88f0ff0480fcfa2db0f960f1e
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2021-12-20 16:00:27 +0530

    Update applog.py

--- applog.py
+++ applog.py
@@ -1,3 +1,11 @@
 from flask import Flask    
 import logging
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello world rajesh is here'
+
+if __name__== "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=80)
