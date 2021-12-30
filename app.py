e538cfa1aea77f3bf0a0e93ce5fd59adab97630a
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2021-12-30 18:26:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import logging
 app = Flask(__name__)
-
+logging.basicConfig(filename='flask.log', level=logging.INFO)
 @app.route('/')
 def hello_world():
-    return 'Hello world rajesh is here'
+    return 'rajesh'
 
-if __name__== "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
