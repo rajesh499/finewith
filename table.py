89ec501f975c7d0bba34d6f5a6ec8ca19a236600
rajesh499 <83573991+rajesh499@users.noreply.github.com>
2022-01-06 16:19:55 +0530

    Update table.py

--- table.py
+++ table.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'mysql2018.cepbobatridn.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'Mysql2018'
 app.config['MYSQL_PASSWORD'] = 'Mysql2018'
-app.config['MYSQL_DB'] ='regform'
+app.config['MYSQL_DB'] ='mydb2'
 
 mydb = mysql.connector.connect(
   host="mysql2018.cepbobatridn.us-east-1.rds.amazonaws.com",
@@ -22,7 +22,7 @@ mycursor.execute("CREATE DATABASE regform")
 mydb = mysql.connector.connect(
   host="mysql2018.cepbobatridn.us-east-1.rds.amazonaws.com",
   password="Mysql2018",
-  database="regform"
+  database="mydb2"
 )
 
 mycursor = mydb.cursor()
